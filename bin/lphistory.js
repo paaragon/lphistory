fd3fe34f8042809e88c76e7e0baa636ba9af6661
PABLO ARAGON MORENO <0015617@insags.com>
2020-02-05 09:27:46 +0100

    clear config

--- bin/lphistory.js
+++ bin/lphistory.js
@@ -1,22 +1,29 @@
 #!/usr/bin/env node
-const argv = require('yargs')
-  .usage('Usage: node index.js -c [conversationid]')
-  .example('node index.js -c 66408b50-37e1-4142-97b6-4e2f4a973bbf -ts 60000', 'Obtener resumen de la conversación restando 1 minuto al timestamp de OAuth')
-  .demandOption(['c'])
-  .alias('c', 'conversationid')
-  .describe('c', 'Id de conversación a buscar')
-  .alias('t', 'timeshift')
-  .describe('t', 'Tiempo de desplazamiento para OAuth con Live Person (ms)')
-  .argv;
-
+const yargs = require('yargs');
 const index = require('../lib/index');
 
-// retrieve cli arguments
-const conversationId = argv.c;
-const timeShift = argv.t;
+yargs.scriptName('lphistory')
+  .command('search [conversationid]', 'Search for conversation', (yargs) => {
+    yargs.positional('conversationid', {
+      type: 'string',
+      describe: 'Conversation id for search'
+    }).describe('t', 'Time shift for Live Person OAuth timestamp')
+      .alias('t', 'time--shift');
+  }, searchConversation)
+  .command('clear-config', 'Clear configuration', () => { }, clearConfiguration)
+  .help()
+  .argv;
 
-(async () => {
+async function searchConversation(argv) {
+  const conversationId = argv.c;
+  const timeShift = argv.t;
   await index.configProcess();
   await index.getLpHistory(conversationId, timeShift);
   process.exit(0);
-})();
+}
+
+function clearConfiguration(argv) {
+  index.clearConfig();
+  console.log('Configuration cleared');
+  process.exit(0);
+}
